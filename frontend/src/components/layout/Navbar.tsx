7c61d669e294e476d848ad83fae96f79f797fa4c
Md.Sayeeduzzaman <157827714+sayeedzaman@users.noreply.github.com>
2026-01-08 20:48:42 +0600

    mobile view logo incereased

--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -257,7 +257,7 @@ export default function Navbar() {
                   alt="Khali's Beauty"
                   width={1000}
                   height={250}
-                  className="h-16 w-auto md:h-40"
+                  className="h-30 w-auto md:h-40"
                   priority
                 />
               </Link>
